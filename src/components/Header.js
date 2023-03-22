4bca87effa4abc1a1ac5ade20bd0ef6f52b4c589
alenamartinkova <martinkova.a@gmail.com>
2023-03-22 10:54:12 +0100

    WIP

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,22 @@ import { faFacebook, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 export default function Header() {
     return (
         <header id={'header'}>
-            <div className={'header--content'}>
+            <div className={'header--content header--content__intro'}>
                 <h1>👋 Hello, I am Alena Martinková</h1>
                 <div>
                     <p> 👀 I am full stack web developer, more inclined to front-end development</p>
-                    <p> 💻 I am currently improving in JavaScript frameworks</p>
+                    <p> 💻 I am currently improving my skills in JavaScript frameworks</p>
                 </div>
             </div>
 
             <div className={'header--content header--content__buttons'}>
-                <a className={'button-primary'}>Contact me!</a>
-                <a href="https://www.facebook.com/martinkova.a">
+                <a href={'mailto:martinkova.a@gmail.com'} className={'button-primary'}>
+                    Contact me!
+                </a>
+                <a href={'https://www.facebook.com/martinkova.a'}>
                     <FontAwesomeIcon className="fa" icon={faFacebook}/>
                 </a>
-                <a href="https://www.linkedin.com/in/alena-martinkova/">
+                <a href={'https://www.linkedin.com/in/alena-martinkova/'}>
                     <FontAwesomeIcon className="fa" icon={faLinkedin} />
                 </a>
             </div>
