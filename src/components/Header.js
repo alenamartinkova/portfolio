79049aad8b9d26c90507dac19be98fb2878bd8a3
alenamartinkova <martinkova.a@gmail.com>
2024-08-15 14:04:56 +0200

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ export default function Header() {
             <div className={'header--content header--content__intro'}>
                 <h1>👋 Hello, I am Alena Martinková</h1>
                 <div>
-                    <p> 👀 I am full stack web developer, more inclined to front-end development</p>
-                    <p> 💻 I am currently improving my skills in JavaScript frameworks and Python on back-end</p>
+                    <p> 💻 I am full stack web developer, currently working as back-end Python lead developer</p>
+                    <p> 👀 I am missing front-end development at the moment tho 😔</p>
                 </div>
             </div>
 
